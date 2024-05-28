92310851c16cb25536c7396fe116f9f188e23e42
Mateusz Skolozynski <mateusz.skolozynski15@gmail.com>
2024-05-28 12:30:07 +0200

    Initialize project'

---
+++ EntityFrameworkApp/EntityFrameworkApp/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
